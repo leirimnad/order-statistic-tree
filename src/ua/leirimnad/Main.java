ba240619c41bb41845dfbb3cb7f031ba91dc2799
leirimnad <leirimnad.web@gmail.com>
2022-02-21 11:37:57 +0200

    Initial commit

---
+++ src/ua/leirimnad/Main.java
@@ -0,0 +1,8 @@
+package ua.leirimnad;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I will become an order statistic tree. Sooner or later...");
+    }
+}
